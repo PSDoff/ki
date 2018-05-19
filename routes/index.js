4e2a23c7cc4d0738b2103d4d3ce78d6d31f83a7b
asomerville <asomerville@demacmedia.com>
2018-05-18 23:01:44 -0400

    Adds SerialPort to read flow sensor data. TODO: Parse dis shit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/cu.wchusbserial1440', {
+    baudRate: 112500
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Keg Intelligentsia' });
 });
 
-module.exports = router;
+setInterval(function() {
+    port.write('/');
+}, 1000);
+
+// Switches the port into "flowing mode"
+port.on('data', function (data) {
+  console.log(data.toString('hex'));
+});
+
+// Open errors will be emitted as an error event
+port.on('error', function(err) {
+  console.log('Error: ', err.message);
+})
+
+module.exports = router, port;
