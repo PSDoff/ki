5d2e95855c76da89e3c2c703f7d91bfebab90d03
Devan Hurst <dhurst@demacmedia.com>
2018-05-19 15:30:07 -0400

    Refactor arduino interface using johnny-five. Provide current keg values to index route.

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var stylus = require('stylus');
+var five = require('johnny-five');
+
+var level0 = 1000;
+var level1 = 1000;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.set('level0', level0);
+app.set('level1', level1);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,6 +45,32 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+var board = new five.Board();
+
+board.on('ready', function() {
+    var sensor0 = new five.Sensor({
+        pin: 2,
+        type: "digital",
+        freq: 1
+    });
+
+    var sensor1 = new five.Sensor({
+        pin: 3,
+        type: "digital",
+        freq: 1
+    });
+
+    sensor0.on('change', function() {
+      level0--;
+      app.set('level0', level0);
+    });
+
+    sensor1.on('change', function() {
+        level1--;
+        app.set('level1', level1);
+    });
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var SerialPort = require('serialport');
-var port = new SerialPort('/dev/cu.wchusbserial1440', {
-    baudRate: 112500
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Keg Intelligentsia' });
+    res.render('index', {
+        title: 'Keg Intelligentsia',
+        level0: req.app.get('level0'),
+        level1: req.app.get('level1')
+    });
 });
 
-setInterval(function() {
-    port.write('/');
-}, 1000);
-
-// Switches the port into "flowing mode"
-port.on('data', function (data) {
-  console.log(data.toString('hex'));
-});
-
-// Open errors will be emitted as an error event
-port.on('error', function(err) {
-  console.log('Error: ', err.message);
-})
-
-module.exports = router, port;
+module.exports = router;
