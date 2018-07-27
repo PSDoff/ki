f09f1ff573f602f3e582b86cb3e736f4a51a0bb8
asomerville <asomerville@demacmedia.com>
2018-07-26 22:35:37 -0400

    Adds in Beer Pour and reducing animation

--- src/views/js/volumeHandler.js
+++ src/views/js/volumeHandler.js
@@ -14,9 +14,12 @@ $(function () {
                 originalVolume = $(tapId).find('[data-volume-original]').attr('data-volume-original'),
                 percentVolume;
 
-            $(tapId).find('[data-volume-remaining]').attr('data-volume-remaining', parseInt(volume) - 1);
+            $(tapId).find('[data-volume-remaining]').attr('data-volume-remaining', parseInt(volume) - 25);
             percentVolume = (volume / originalVolume) * 100;
             console.log("Left tap remaining: " + percentVolume.toFixed(0) + "%");
+
+            $(tapId).find('.liquid').css('height', percentVolume.toFixed(0) + '%');
+            $(tapId).find('.beer-foam').css('bottom', percentVolume.toFixed(0) + '%');
 
             $(tapId).find('[data-volume-percent]').attr('data-volume-percent', percentVolume.toFixed(0));
         } else if (e.which == 87) {
@@ -25,9 +28,12 @@ $(function () {
                 originalVolume = $(tapId).find('[data-volume-original]').attr('data-volume-original'),
                 percentVolume;
 
-            $(tapId).find('[data-volume-remaining]').attr('data-volume-remaining', parseInt(volume) - 1);
+            $(tapId).find('[data-volume-remaining]').attr('data-volume-remaining', parseInt(volume) - 25);
             percentVolume = (volume / originalVolume) * 100;
             console.log("Left tap remaining: " + percentVolume.toFixed(0) + "%");
+
+            $(tapId).find('.liquid').css('height', percentVolume.toFixed(0) + '%');
+            $(tapId).find('.beer-foam').css('bottom', percentVolume.toFixed(0) + '%');
 
             $(tapId).find('[data-volume-percent]').attr('data-volume-percent', percentVolume.toFixed(0));
         }
