1c0cebf84902e34e71fe3b8a78e34e1031e3ecb2
asomerville <asomerville@demacmedia.com>
2018-07-27 22:35:20 -0400

    Revises calculations for percentage remaining Moves data attributes to tap level Adjusts JS to suit

--- src/views/js/beerPourAnimation.js
+++ src/views/js/beerPourAnimation.js
@@ -1,23 +1,24 @@
 // Credit to https://codepen.io/TimRuby/pen/jcLia
 
-$(document).ready(function() {
-  $('.pour') //Pour Me Another Drink, Bartender!
-    .delay(2000)
-    .animate({
-      height: '360px'
-      }, 1500)
-    .delay(1600)
-    .slideUp(500);
+$(document).ready(function () {
 
-  $('.liquid') // I Said Fill 'Er Up!
-    .delay(3400)
-    .animate({
-      height: '100%'
-    }, 2500);
+    $('.pour') //Pour Me Another Drink, Bartender!
+        .delay(2000)
+        .animate({
+            height: '360px'
+        }, 1500)
+        .delay(1600)
+        .slideUp(500);
 
-  $('.beer-foam') // Keep that Foam Rollin' Toward the Top! Yahooo!
-    .delay(3400)
-    .animate({
-      bottom: '100%'
-      }, 2500);
-  });
+    $('.liquid') // I Said Fill 'Er Up!
+        .delay(3400)
+        .animate({
+            height: parseInt($(this).parents('[data-volume-percent]').data('volume-percentage')).toFixed(0) + '%'
+        }, 2500);
+
+    $('.beer-foam') // Keep that Foam Rollin' Toward the Top! Yahooo!
+        .delay(3400)
+        .animate({
+            bottom: parseInt($(this).parents('[data-volume-percent]').data('volume-percentage')).toFixed(0) + '%'
+        }, 2500);
+});
--- src/views/js/volumeHandler.js
+++ src/views/js/volumeHandler.js
@@ -1,36 +1,44 @@
 $(function () {
     var socket = io();
+
     socket.on('update volume', function(data){
         var tapContainer = $(".tap-" + data.key);
 
+        var volume = data.tap.volume,
+            originalVolume = data.tap.initialVolume,
+            percentVolume = ((volume / originalVolume) * 100).toFixed(2);
+
         if (tapContainer.length > 0) {
-            tapContainer.find('.tap__volume').text(data.tap.volume);
+            tapContainer.find('.liquid').css('height', percentVolume + '%');
+            tapContainer.find('.beer-foam').css('bottom', percentVolume + '%');
+            tapContainer.attr('data-volume-percent', percentVolume);
         }
     });
+
     $('body').keydown(function(e){
         if (e.which == 81) {
             let tapId = ".tap-left";
-            var volume = $(tapId).find('[data-volume-remaining]').attr('data-volume-remaining'),
-                originalVolume = $(tapId).find('[data-volume-original]').attr('data-volume-original'),
+            var volume = $(tapId).attr('data-volume-remaining'),
+                originalVolume = $(tapId).attr('data-volume-original'),
                 percentVolume = ((volume / originalVolume) * 100).toFixed(0);
 
             if (percentVolume > 0) {
-              $(tapId).find('[data-volume-remaining]').attr('data-volume-remaining', parseInt(volume) - 25);
+              $(tapId).attr('data-volume-remaining', parseInt(volume) - 25);
               $(tapId).find('.liquid').css('height', percentVolume + '%');
               $(tapId).find('.beer-foam').css('bottom', percentVolume + '%');
-              $(tapId).find('[data-volume-percent]').attr('data-volume-percent', percentVolume);
+              $(tapId).attr('data-volume-percent', percentVolume);
             }
         } else if (e.which == 87) {
             let tapId = ".tap-right";
-            var volume = $(tapId).find('[data-volume-remaining]').attr('data-volume-remaining'),
-                originalVolume = $(tapId).find('[data-volume-original]').attr('data-volume-original'),
-                percentVolume = ((volume / originalVolume) * 100).toFixed(0);;
+            var volume = $(tapId).attr('data-volume-remaining'),
+                originalVolume = $(tapId).attr('data-volume-original'),
+                percentVolume = ((volume / originalVolume) * 100).toFixed(0);
 
             if (percentVolume > 0) {
-              $(tapId).find('[data-volume-remaining]').attr('data-volume-remaining', parseInt(volume) - 25);
+              $(tapId).attr('data-volume-remaining', parseInt(volume) - 25);
               $(tapId).find('.liquid').css('height', percentVolume + '%');
               $(tapId).find('.beer-foam').css('bottom', percentVolume + '%');
-              $(tapId).find('[data-volume-percent]').attr('data-volume-percent', percentVolume);
+              $(tapId).attr('data-volume-percent', percentVolume);
             }
         }
     })
