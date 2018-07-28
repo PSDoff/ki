51c3040273cb87c5787fec2813d6a1a9b3ea2408
asomerville <asomerville@demacmedia.com>
2018-07-28 13:47:05 -0400

    Reworks layout for large poster. Adds bubble overlay during pour

--- src/assets/js/volumeHandler.js
+++ src/assets/js/volumeHandler.js
@@ -1,7 +1,7 @@
 $(function () {
     var socket = io();
 
-    socket.on('update volume', function(data){
+    socket.on('update volume', function (data) {
         var tapContainer = $(".tap-" + data.key);
 
         var volume = data.tap.volume,
@@ -9,13 +9,17 @@ $(function () {
             percentVolume = ((volume / originalVolume) * 100).toFixed(2);
 
         if (tapContainer.length > 0) {
-            tapContainer.find('.liquid').css('height', percentVolume + '%');
-            tapContainer.find('.beer-foam').css('bottom', percentVolume + '%');
+            tapContainer.find('.beer-container').addClass('pouring');
+            setTimeout(function () {
+                tapContainer.find('.beer-container').removeClass('pouring');
+            }, 2500);
+            // tapContainer.find('.liquid').css('height', percentVolume + '%');
+            // tapContainer.find('.beer-foam').css('bottom', percentVolume + '%');
             tapContainer.attr('data-volume-percent', percentVolume);
         }
     });
 
-    $('body').keydown(function(e){
+    $('body').keydown(function (e) {
         if (e.which == 81) {
             let tapId = ".tap-left";
             var volume = $(tapId).attr('data-volume-remaining'),
@@ -23,10 +27,15 @@ $(function () {
                 percentVolume = ((volume / originalVolume) * 100).toFixed(0);
 
             if (percentVolume > 0) {
-              $(tapId).attr('data-volume-remaining', parseInt(volume) - 25);
-              $(tapId).find('.liquid').css('height', percentVolume + '%');
-              $(tapId).find('.beer-foam').css('bottom', percentVolume + '%');
-              $(tapId).attr('data-volume-percent', percentVolume);
+                $(tapId).attr('data-volume-remaining', parseInt(volume) - 25);
+                // $(tapId).find('.liquid').css('height', percentVolume + '%');
+                // $(tapId).find('.beer-foam').css('bottom', percentVolume + '%');
+                $(tapId).attr('data-volume-percent', percentVolume);
+
+                $(tapId).find('.beer-container').addClass('pouring');
+                setTimeout(function () {
+                    $(tapId).find('.beer-container').removeClass('pouring');
+                }, 2500);
             }
         } else if (e.which == 87) {
             let tapId = ".tap-right";
@@ -35,10 +44,15 @@ $(function () {
                 percentVolume = ((volume / originalVolume) * 100).toFixed(0);
 
             if (percentVolume > 0) {
-              $(tapId).attr('data-volume-remaining', parseInt(volume) - 25);
-              $(tapId).find('.liquid').css('height', percentVolume + '%');
-              $(tapId).find('.beer-foam').css('bottom', percentVolume + '%');
-              $(tapId).attr('data-volume-percent', percentVolume);
+                $(tapId).attr('data-volume-remaining', parseInt(volume) - 25);
+                // $(tapId).find('.liquid').css('height', percentVolume + '%');
+                // $(tapId).find('.beer-foam').css('bottom', percentVolume + '%');
+                $(tapId).attr('data-volume-percent', percentVolume);
+
+                $(tapId).find('.beer-container').addClass('pouring');
+                setTimeout(function(){
+                    $(tapId).find('.beer-container').removeClass('pouring');
+                }, 2500);
             }
         }
     })
