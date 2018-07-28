7e24e2fd6032fdcba736bdb26bbf8c60f09953ac
asomerville <asomerville@demacmedia.com>
2018-07-28 03:02:20 -0400

    Refines initial beer pouring animation to work with current values

--- src/assets/js/beerPourAnimation.js
+++ src/assets/js/beerPourAnimation.js
@@ -1,25 +1,28 @@
 // Credit to https://codepen.io/TimRuby/pen/jcLia
 
-$(document).ready(function () {
+$(function () {
 
-    $('.pour') //Pour Me Another Drink, Bartender!
-        .delay(2000)
-        .animate({
-            height: '360px'
-        }, 1500)
-        .delay(1600)
-        .slideUp(500);
+    $('[class*="tap-"]').each(function() {
 
-    $('.liquid') // I Said Fill 'Er Up!
-        .delay(3400)
-        .animate({
-            // height: parseInt($(this).parents('[data-volume-percent]').data('volume-percentage')).toFixed(0) + '%'
-            height: '100%';
-        }, 2500);
+        $(this).find('.pour') //Pour Me Another Drink, Bartender!
+            .delay(1200)
+            .animate({
+                height: '100%'
+            }, 1500)
+            .delay(1000)
+            .slideUp(500);
 
-    $('.beer-foam') // Keep that Foam Rollin' Toward the Top! Yahooo!
-        .delay(3400)
-        .animate({
-            bottom: parseInt($(this).parents('[data-volume-percent]').data('volume-percentage')).toFixed(0) + '%'
-        }, 2500);
+        $(this).find('.liquid') // I Said Fill 'Er Up!
+            .delay(4100)
+            .animate({
+                height: parseInt($(this).data('volumePercent')).toFixed(0) + '%'
+            }, 2500);
+
+        $(this).find('.beer-foam') // Keep that Foam Rollin' Toward the Top! Yahooo!
+            .delay(4100)
+            .animate({
+                bottom: parseInt($(this).data('volumePercent')).toFixed(0) + '%'
+            }, 2500);
+    })
+
 });
